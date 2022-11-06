d3ad12beea1dd7d41a34f7bf35afadfe6a94112c
Hau Tran <hautv@vuihoc.vn>
2022-11-06 14:29:55 +0700

    Export spine component

--- lib/flame_spine.dart
+++ lib/flame_spine.dart
@@ -7,6 +7,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:spine_core/spine_core.dart' as core;
 
+export 'src/spine_component.dart';
+
 part 'src/asset_loader.dart';
 part 'src/skeleton_animation.dart';
 part 'src/texture.dart';
